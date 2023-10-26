fca0a914975f34b19fa2ac73ccd610c0bd957610
Dasajton <david.sajitz.94@gmail.com>
2023-10-26 11:48:44 +0200

    changed url

--- script.js
+++ script.js
@@ -54,7 +54,7 @@ btnRoll.addEventListener("click", function () {
 
     // 2. Display the Dice on Screen
     diceEl.classList.remove("hidden");
-    diceEl.src = `dice-${dice}.png`;
+    diceEl.src = `./images/dice-${dice}.png`;
 
     // 3. Check for rolled 1:
     if (dice !== 1) {
